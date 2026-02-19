aea2605d9c1ec9a0d2e1147e85954f37bbf9fb79
Louis Lin <yuhui.lin@scsstudent.org>
2026-02-19 15:32:37 -0800

    complete video 3

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,32 @@
-import { Stack } from "expo-router";
+import { Tabs } from "expo-router";
+import Entypo from "@expo/vector-icons";
+import { Feather } from "@expo/vector-icons";
+import { Colors } from "react-native/Libraries/NewAppScreen";
+import { theme } from "../theme";
 
 export default function Layout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ title: "Home" }} />
-    </Stack>
+    <Tabs screenOptions={{ tabBarActiveTintColor: theme.colorGreen }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarShowLabel: false,
+          tabBarIcon: ({ size, color }) => (
+            <Entypo name="leaf" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          tabBarShowLabel: false,
+          tabBarIcon: ({ size, color }) => (
+            <Feather name="user" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
   );
 }
