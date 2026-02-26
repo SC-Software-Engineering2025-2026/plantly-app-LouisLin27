a6dc6fafd399d6152ff0a1749bf5b4bc58c8a288
Louis Lin <yuhui.lin@scsstudent.org>
2026-02-26 00:31:06 -0800

    complete video 8

--- theme.ts
+++ theme.ts
@@ -1,6 +1,8 @@
 export const theme = {
   spacing12: 12,
   colorGreen: "#29b365",
+  colorAppleGreen: "#a0d36c",
+  colorLimeGreen: "#d0e57e",
   colorLeafyGreen: "#206a42",
   colorWhite: "#fff",
 };
