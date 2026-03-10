22529ca01d706687a640339d5d82914adaed12c9
Louis Lin <yuhui.lin@scsstudent.org>
2026-03-10 04:45:31 -0700

    complete video 16-19 (although the android didnt work)

--- app/[tabs]/_layout.tsx
+++ app/[tabs]/_layout.tsx
@@ -1,18 +1,15 @@
-import { Link, Redirect, Tabs } from "expo-router";
+import { Redirect, SplashScreen, Tabs } from "expo-router";
 import { Entypo } from "@expo/vector-icons";
 import { Feather } from "@expo/vector-icons";
 import { theme } from "@/theme";
 import { useUserStore } from "@/store/userStore";
-import { AntDesign } from "@expo/vector-icons";
-import { Pressable } from "react-native";
-
-const hasFinishedOnboarding = true;
 
 export default function Layout() {
+  SplashScreen.hideAsync();
   const hasFinishedOnborading = useUserStore(
     (state) => state.hasFinishedOnboarding,
   );
-  if (!hasFinishedOnboarding) {
+  if (!hasFinishedOnborading) {
     return <Redirect href="/onboarding" />;
   }
   return (
